132b5830509dc7eb8a1e98dcd46868d49e2e3e55
cipher <asaforbrian18@gmail.com>
2023-08-04 23:52:29 +0300

    Created main file with starting code

---
+++ main.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+from tkinter import messagebox
+import ttkbootstrap as tb
+from ttkbootstrap.toast import ToastNotification
+import sqlite3
+
+root = tb.Window(themename="superhero")
+root.title("Student Management + Registration System")
+root.geometry("500x600")
+
+
+
+root.mainloop()
